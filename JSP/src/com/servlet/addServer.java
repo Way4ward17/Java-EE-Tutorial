9967bb38cfdac0cdf686356ade01e171ae15846e
way4ward17 <supersondultimate@gmail.com>
2020-04-07 10:45:01 +0100

    Updating my code

--- JSP/src/com/servlet/addServer.java
+++ JSP/src/com/servlet/addServer.java
@@ -1,7 +1,25 @@
 package com.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class addServer extends HttpServlet{
+	
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		int sum = a + b;
+		
+		//REQUEST DISPATCHER
+		req.setAttribute("sum", sum);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("secondServlet");
+		dispatcher.forward(req, res);
+	}
 
 }
--- JSP/src/com/servlet/secondServlet.java
+++ JSP/src/com/servlet/secondServlet.java
@@ -1,5 +1,26 @@
 package com.servlet;
 
-public class secondServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+public class secondServlet extends HttpServlet{
+	
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	
+
+		int sum = (int) req.getAttribute("sum");
+		
+		PrintWriter writer = res.getWriter();
+		writer.println("Result is: "+sum);
+	}
 
 }
